d36f6ed713ace1e50b98e1c7d3a553078df80266
Jeff Wong <awole20@gmail.com>
2022-06-30 14:35:59 -0700

    init

---
+++ plugin.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# name: discourse-alias
+# about: Support for account aliasing
+# version: 0.1
+# authors: Jeff Wong
+# transpile_js: true
+
+after_initialize do
+end
